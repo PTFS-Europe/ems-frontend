4fe376aa28cdc16c16bdf7ec00209a738cdd2e6b
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-05-06 14:18:04 +0100

    Remove unused import
    Removed unused useDispatch import from the QueryList component

--- src/components/Ems/EnquiriesContainer/Queries/QueryList/QueryList.test.js
+++ src/components/Ems/EnquiriesContainer/Queries/QueryList/QueryList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { render } from '@testing-library/react';
 
 import QueryList from './QueryList';
