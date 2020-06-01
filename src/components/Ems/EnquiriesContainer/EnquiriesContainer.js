c76278f37f3d1832babe95cd46a40031bf6b7e6a
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-06-01 17:57:36 +0100

    Remove SideDrawer component from use
    This component was created before an approach to mobile navigation had
    been decided, it is now redundant. I am keeping the component and the
    associated background component for now as they may be useful

--- src/components/Ems/EnquiriesContainer/EnquiriesContainer.js
+++ src/components/Ems/EnquiriesContainer/EnquiriesContainer.js
@@ -4,14 +4,12 @@ import styles from './EnquiriesContainer.module.scss';
 
 import Queries from './Queries/Queries';
 import ActiveQuery from './Queries/ActiveQuery/ActiveQuery';
-import SideDrawer from '../../UI/SideDrawer/SideDrawer';
 
 const EnquiriesContainer = (props) => {
     return (
         <div className={styles.queriesContainer}>
             <Queries></Queries>
             <ActiveQuery></ActiveQuery>
-            <SideDrawer></SideDrawer>
         </div>
     );
 };
