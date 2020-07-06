1e88ca725b660b21e17bd0556505d290c204e6cd
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-07-06 12:09:22 +0100

    Generic ColourPicker component
    This commit adds a generic <ColourPicker> component which allows the
    display of a number of arbitrary icons in a predefined range of colours,
    clicking an icon causes a passed callback to be called. Ordinarily, this
    callback will be a function to update some state or dispatch an action.
    
    Includes:
    
    - <ColourPicker> component
    - Unit tests

---
+++ src/components/UI/ColourPicker/ColourPicker.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Popover from '../Popover/Popover';
+
+import styles from './ColourPicker.module.scss';
+
+const colours = [
+    '#c65050',
+    '#d69d3f',
+    '#d7de38',
+    '#8ebf65',
+    '#39d04b',
+    '#40d1b5',
+    '#5090c6',
+    '#4157d0',
+    '#9050c6',
+    '#d345c2'
+];
+
+const ColourPicker = ({ updateColour, icon }) => {
+    return (
+        <Popover instanceStyles={styles.popover}>
+            <div className={styles.swatchContainer}>
+                {colours.map((colour) => (
+                    <button
+                        key={colour}
+                        onClick={() => updateColour({ colour })}
+                        className={styles.swatch}
+                    >
+                        {icon(colour)}
+                    </button>
+                ))}
+            </div>
+        </Popover>
+    );
+};
+
+ColourPicker.propTypes = {
+    updateColour: PropTypes.func.isRequired,
+    icon: PropTypes.func.isRequired
+};
+
+export default ColourPicker;
---
+++ src/components/UI/ColourPicker/ColourPicker.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import ColourPicker from './ColourPicker';
+
+// Our mock props
+const updateColour = jest.fn();
+const icon = jest
+    .fn()
+    .mockImplementation((colour) => (
+        <div data-testid={'mockIcon'}>{colour}</div>
+    ));
+
+describe('ColourPicker', () => {
+    let q;
+    beforeEach(() => {
+        q = render(
+            <ColourPicker
+                updateColour={updateColour}
+                icon={icon}
+            ></ColourPicker>
+        );
+    });
+    test('wrapping popover is rendered', () => {
+        const popover = q.getByRole('dialog');
+        expect(popover).toBeInTheDocument();
+    });
+    test('one button for each colour is rendered', () => {
+        const buttons = q.getAllByRole('button');
+        expect(buttons).toHaveLength(10);
+    });
+    test('clicking a button causes the passed callback to be called', () => {
+        const buttons = q.getAllByRole('button');
+        fireEvent.click(buttons[0]);
+        expect(updateColour).toHaveBeenCalled();
+    });
+    test('the passed icon function is called with the icons colour', () => {
+        const icon = q.getByText('#c65050');
+        expect(icon).toBeTruthy();
+    });
+});
