1679d46287ce4706f50eedd4eb3b3d3ff93018db
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2020-12-10 11:34:07 +0000

    Add sentry.io
    This commits integrates sentry.io error tracking. This is by no means a
    commitment to sentry.io, I am currently just using a Developer account,
    but it will be interesting to see how much value it provides.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import './i18n';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
 import './index.scss';
 import { store } from './store';
 import api from './classes/EmsApi';
+
+// Initialise our Sentry tracing
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    autoSessionTracking: true,
+    integrations: [
+        new Integrations.BrowserTracing()
+    ],
+    tracesSampleRate: 1.0
+});
 
 // Initialise our API request and response interceptors
 api.requestInterceptor();
